a29214d677b2382408715e429740a2993ddd581d
erik1110 <oceanuheart@gmail.com>
2021-05-19 11:43:29 +0800

    [refactor] 修改user_id格式

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 # 定義 Users 模型
 class Users(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.String(255), primary_key = True)
     username = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     sex = db.Column(db.String(1), nullable=False)
