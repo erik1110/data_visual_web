14c1d7532cedd9477242270009bf2af8425f93b7
Erik <40282726+erik1110@users.noreply.github.com>
2021-05-20 10:34:29 +0800

    [delete] needless

--- app/.ipynb_checkpoints/views-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from config import Config, ProductionConfig, DevelopmentConfig, TestingConfig
-from flask import Flask
-
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-
-@app.route('/')
-def index():
-    return 'My Flask App!'
